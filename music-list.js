fa6f89b4fa59ffc9c265dd1aab8218bb93f4ff8e
Melisha Shakya <melissashakya1697@gmail.com>
2023-10-12 12:55:06 +0545

    first commit

---
+++ music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+  {
+    name: "Fearless",
+    artist: "Taylor Swift",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Bad Blood",
+    artist: "Taylor Swift",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "Single Soon",
+    artist: "Selena Gomez",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Stuck With You",
+    artist: "Ariana Grande ft Justin Bieber",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Perfect",
+    artist: "One Direction",
+    img: "music-5",
+    src: "music-5",
+  },
+];
